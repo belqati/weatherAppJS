e33cb2c55b88374c627ab8ccad95f045cb2a3bb9
belqati <nahnahgames@gmail.com>
2018-09-22 12:38:02 -0500

    add fetch weather

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('app.js is live')
+// get current location
+// let lat;
+// let lon;
+
+// navigator.geolocation.getCurrentPosition(function(position) {
+//   lat = position.coords.latitude;
+//   lon = position.coords.longitude;
+  
+//   console.log(position.coords.latitude, position.coords.longitude);
+// });
+
+// initialize weather object
+const weather = new Weather(42.199422999999996, -87.8143274);
+
+weather.getWeather()
+  .then(results => {
+    console.log(results);
+  })
+  .catch(err => console.log(err));
+
--- public/scripts/weather.js
+++ public/scripts/weather.js
@@ -1,1 +1,24 @@
-console.log('weather.js is live')
+class Weather {
+  constructor(lat, lon){
+    this.apiKey = process.env.DARKSKY_SECRET;
+    this.lat = lat;
+    this.lon = lon;
+  }
+
+  // fetch weather from API
+  // if CORS need disabling for testing, close Chrome and start it via CLI: "chrome --disable-web-security --user-data-dir"
+  async getWeather(){
+    const response = await fetch(`https://api.darksky.net/forecast/${this.apiKey}/${this.lat},${this.lon}`);
+
+    const responseData = await response.json();
+
+    // limit response to current conditions
+    return responseData;
+  }
+
+  // change weather location
+  changeLocation(lat, lon){
+    this.lat = lat;
+    this.lon = lon;
+  }
+}
