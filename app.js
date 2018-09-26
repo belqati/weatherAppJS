651571906731e92031d2e3244e1d316e9b4fab98
belqati <nahnahgames@gmail.com>
2018-09-26 12:57:39 -0500

    add local storage functionality

--- app.js
+++ app.js
@@ -9,21 +9,41 @@
 //   // console.log(lat, lon);
 // });
 
-// initialize weather object
-const weather = new Weather(50, -50);
+// init localStorage
+const storage = new Storage();
+// get stored location data
+const weatherLocation = storage.getLocationData();
+// init weather object
+const weather = new Weather(weatherLocation.lat, weatherLocation.lon);
 // init UI
 const ui = new UI();
 
 // get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation(20, 80);
+// change location event
+document.querySelector('#w-change-btn').addEventListener('click', (e) => {
+  const lat = document.querySelector('#lat').value;
+  const lon = document.querySelector('#lon').value;
+
+  // change location
+  weather.changeLocation(lat, lon);
+
+  // set location to localStorage
+  storage.setLocationData(lat, lon);
+
+  // get and display weather
+  getWeather();
+
+  // close modal (must use JQuery for Bootstrap modal)
+  $('#locModal').modal('hide');
+});
 
 function getWeather(){
-weather.getWeather()
-  .then(results => {
-    ui.paint(results);
-    console.log(results);
-  })
-  .catch(err => console.log(err));
+  weather.getWeather()
+    .then(results => {
+      ui.paint(results);
+      console.log(results);
+    })
+    .catch(err => console.log(err));
 }
